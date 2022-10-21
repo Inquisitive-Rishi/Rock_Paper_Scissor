d60f80e2f4c7f4c8e118d070f31f889217b9dbad
Inquisitive-Rishi <rishiraj007bgp@gmail.com>
2022-10-21 12:12:54 +0530

    add result message

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+//random choise of computer
 const choices = ["rock", "paper", "scissor"];
 
 const getComputerChoice = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-console.log(getComputerChoice(choices));
+// console.log(getComputerChoice(choices));
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "It's a tie.";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "You loose! Paper beats Rock.";
+  } else if (playerSelection == "rock" && computerSelection == "scissor") {
+    return "You win! Rock beats Scissor.";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win! Paper beats Rock.";
+  } else if (playerSelection == "paper" && computerSelection == "scissor") {
+    return "You loose! Scissor beats Paper.";
+  } else if (playerSelection == "scissor" && computerSelection == "rock") {
+    return "You loose! Rock beats Scissor.";
+  } else if (playerSelection == "scissor" && computerSelection == "paper") {
+    return "You win! Scissor beats Paper.";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(choices);
+console.log(playRound(playerSelection, computerSelection));
